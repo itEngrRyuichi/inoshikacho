6ceb1aaf26a2cbc30d00f2647fc3da24051abafd
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-05-19 02:27:18 +0900

    added destroy

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,17 @@ Route::get('stores/{store_id}/plans/create', 'StorePlanController@create');
 Route::post('stores/{store_id}/plans', 'StorePlanController@store');
 Route::get('stores/{store_id}/plans/{id}/edit', 'StorePlanController@edit');
 Route::put('stores/{store_id}/plans/{id}', 'StorePlanController@update');
+Route::delete('stores/{store_id}/plans/{id}', 'StorePlanController@destroy');
 /* 部屋 */
 Route::get('stores/{store_id}/rooms/create', 'StoreRoomController@create');
 Route::post('stores/{store_id}/rooms', 'StoreRoomController@store');
 Route::get('stores/{store_id}/rooms/{id}/edit', 'StoreRoomController@edit');
 Route::put('stores/{store_id}/rooms/{id}', 'StoreRoomController@update');
+Route::delete('stores/{store_id}/rooms/{id}', 'StoreRoomController@destroy');
 /* コメント */
 Route::get('stores/{store_id}/comments/create', 'StoreCommentController@create');
 Route::post('stores/{store_id}/comments', 'StoreCommentController@store');
+Route::delete('stores/{store_id}/comments/{id}', 'StoreCommentController@destroy');
 
 /* ----- 予約関係 ----- */
 Route::resource('reserves', 'HomeController');
