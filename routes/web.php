ab2cf1c37c0acde88349e3006bc219d025470c0b
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-05-19 02:23:56 +0900

    inside of route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* ----- ホーム画面 ----- */
+Route::get('/', 'HomeController@index');
+
+/* ----- ユーザー関係 ----- */
+Route::resource('users', 'UserController');
+
+/* ----- 店舗関係 ----- */
+Route::resource('stores', 'StoreController');
+/* プラン */
+Route::get('stores/{store_id}/plans/create', 'StorePlanController@create');
+Route::post('stores/{store_id}/plans', 'StorePlanController@store');
+Route::get('stores/{store_id}/plans/{id}/edit', 'StorePlanController@edit');
+Route::put('stores/{store_id}/plans/{id}', 'StorePlanController@update');
+/* 部屋 */
+Route::get('stores/{store_id}/rooms/create', 'StoreRoomController@create');
+Route::post('stores/{store_id}/rooms', 'StoreRoomController@store');
+Route::get('stores/{store_id}/rooms/{id}/edit', 'StoreRoomController@edit');
+Route::put('stores/{store_id}/rooms/{id}', 'StoreRoomController@update');
+/* コメント */
+Route::get('stores/{store_id}/comments/create', 'StoreCommentController@create');
+Route::post('stores/{store_id}/comments', 'StoreCommentController@store');
+
+/* ----- 予約関係 ----- */
+Route::resource('reserves', 'HomeController');
