4ea57d03e48af59d7a7c3f1fc90c0f1ff1728025
sigayuusuke <yusuke.mczk0619@gmail.com>
2021-05-19 14:47:09 +0900

    Roomモデルの作成、およびrooms_tableのマイグレーションファイルの作成

---
+++ database/migrations/2021_05_19_053103_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('room_id');
+            $table->string('room_name',50);
+            $table->integer('capacity');
+            $table->integer('amenity_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('amenity_id')->references('id')->on('amenities')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
