32cdb00e0202f76fbb4e2be85974619a1c8f1855
新田野乃華 <b1017177@gmail.com>
2021-05-19 17:06:54 +0900

    person_typesテーブルのseederファイル作成

---
+++ database/seeders/PersonTypesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PersonTypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('person_types')->insert(['person_types'=>'子供']);
+        DB::table('person_types')->insert(['person_types'=>'大人']);
+    }
+}
