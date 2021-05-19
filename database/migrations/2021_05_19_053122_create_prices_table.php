2a7227f99247f3aa9e99756a03b1b08ebb89d4ad
ami-2021php <2021php41-student015@la-study.com>
2021-05-19 14:53:19 +0900

    price_tabel の作成

---
+++ database/migrations/2021_05_19_053122_create_prices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('person_type_id')->unsigned()->index();
+            $table->bigInteger('plan_id')->unsigned()->index();
+            $table->integer('price');
+            $table->date('created_at');
+            $table->date('updated_at');
+            $table->timestamps();
+
+            $table->foreign('person_type_id')->references('id')->on('person_type_table')->onDelete('cascade');
+            $table->foreign('plan_id')->references('id')->on('plans_table')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
