a47e0d5e7a9ad1391baed811b2ae544daf199d14
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-05-19 11:16:07 +0900

    comment out

--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function(){
+    return view('welcome');
+});
+
 /* ----- ホーム画面 ----- */
-Route::get('/', 'HomeController@index');
+/* Route::get('/', 'HomeController@index'); */
 
 /* ----- ユーザー関係 ----- */
-Route::resource('users', 'UserController');
+/* Route::resource('users', 'UserController'); */
 
 /* ----- 店舗関係 ----- */
-Route::resource('stores', 'StoreController');
+/* Route::resource('stores', 'StoreController'); */
 /* プラン */
-Route::get('stores/{store_id}/plans/create', 'StorePlanController@create');
+/* Route::get('stores/{store_id}/plans/create', 'StorePlanController@create');
 Route::post('stores/{store_id}/plans', 'StorePlanController@store');
 Route::get('stores/{store_id}/plans/{id}/edit', 'StorePlanController@edit');
 Route::put('stores/{store_id}/plans/{id}', 'StorePlanController@update');
-Route::delete('stores/{store_id}/plans/{id}', 'StorePlanController@destroy');
+Route::delete('stores/{store_id}/plans/{id}', 'StorePlanController@destroy'); */
 /* 部屋 */
-Route::get('stores/{store_id}/rooms/create', 'StoreRoomController@create');
+/* Route::get('stores/{store_id}/rooms/create', 'StoreRoomController@create');
 Route::post('stores/{store_id}/rooms', 'StoreRoomController@store');
 Route::get('stores/{store_id}/rooms/{id}/edit', 'StoreRoomController@edit');
 Route::put('stores/{store_id}/rooms/{id}', 'StoreRoomController@update');
-Route::delete('stores/{store_id}/rooms/{id}', 'StoreRoomController@destroy');
+Route::delete('stores/{store_id}/rooms/{id}', 'StoreRoomController@destroy'); */
 /* コメント */
-Route::get('stores/{store_id}/comments/create', 'StoreCommentController@create');
+/* Route::get('stores/{store_id}/comments/create', 'StoreCommentController@create');
 Route::post('stores/{store_id}/comments', 'StoreCommentController@store');
-Route::delete('stores/{store_id}/comments/{id}', 'StoreCommentController@destroy');
+Route::delete('stores/{store_id}/comments/{id}', 'StoreCommentController@destroy'); */
 
 /* ----- 予約関係 ----- */
-Route::resource('reserves', 'HomeController');
+/* Route::resource('reserves', 'HomeController'); */
