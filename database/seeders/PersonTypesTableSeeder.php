cb2bd0038c22e4fe205d1a2e14eb2e14eb39c3bb
新田野乃華 <b1017177@gmail.com>
2021-05-20 11:11:27 +0900

    タイプを追加

--- database/seeders/PersonTypesTableSeeder.php
+++ database/seeders/PersonTypesTableSeeder.php
@@ -15,7 +15,9 @@ class PersonTypesTableSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('person_types')->insert(['person_types'=>'子供']);
-        DB::table('person_types')->insert(['person_types'=>'大人']);
+        DB::table('person_types')->insert(['person_types'=>'大人(13歳以上)']);
+        DB::table('person_types')->insert(['person_types'=>'小学生(7歳から12歳まで)']);
+        DB::table('person_types')->insert(['person_types'=>'未就学児(4歳から6歳まで)']);
+        DB::table('person_types')->insert(['person_types'=>'幼児(0歳から3歳まで)']);
     }
 }
