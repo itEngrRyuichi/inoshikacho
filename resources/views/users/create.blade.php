8501609e6df60e9767aeaa197d39da6c0a9d62fe
ami-2021php <2021php41-student015@la-study.com>
2021-05-20 16:45:22 +0900

    indexとcreateの編集

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,4 +1,39 @@
 @extends('layout.app')
 @section('content')
 {{-- ここに書いてね～ --}}
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">名前</span>
+  <input type="text" class="form-control" placeholder="猿飛アスマ" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">年齢</span>
+  <input type="text" class="form-control" placeholder="22" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">住所</span>
+  <input type="text" class="form-control" placeholder="東京都新宿区" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">電話番号</span>
+  <input type="text" class="form-control" placeholder="1234-5678" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="abcde" aria-label="Username">
+  <span class="input-group-text">@</span>
+  <input type="text" class="form-control" placeholder="co.jp" aria-label="Server">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">パスワード</span>
+  <input type="text" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="d-grid gap-2 col-6 mx-auto">
+  <button class="btn btn-primary" type="button">新規登録</button>
+</div>
+
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,46 @@
 @extends('layout.app')
 @section('content')
 {{-- ここに書いてね～ --}}
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">会員番号</th>
+      <th scope="col">名前</th>
+      <th scope="col">年齢</th>
+      <th scope="col">Eメールアドレス</th>
+      <th scope="col">電話番号</th>
+      <th scope="col">住所</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>山田</td>
+      <td>22</td>
+      <td>Laravel@co.jp</td>
+      <td>1234-5678</td>
+      <td>東京都新宿区</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>鈴木</td>
+      <td>22</td>
+      <td>abcdefg@co.jp</td>
+      <td>1111-2222</td>
+      <td>東京都新宿区</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+
+<div class="d-grid gap-2 col-6 mx-auto">
+  <button class="btn btn-primary" type="button">新規登録</button>
+</div>
 @endsection
