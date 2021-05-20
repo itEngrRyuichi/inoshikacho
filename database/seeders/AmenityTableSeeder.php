7bd000dc117c6ac9ec0b803a5d80f5eaab1f20c4
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-05-20 10:51:13 +0900

    added seeder

---
+++ database/seeders/AmenityTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AmenityTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('amenities')->insert(['amenity_name' => 'シングルベッド']);
+        DB::table('amenities')->insert(['amenity_name' => 'ダブルベッド']);
+        DB::table('amenities')->insert(['amenity_name' => 'ツインベッド']);
+        DB::table('amenities')->insert(['amenity_name' => 'セミダブル']);
+        DB::table('amenities')->insert(['amenity_name' => '布団']);
+        DB::table('amenities')->insert(['amenity_name' => 'マットレス']);
+        DB::table('amenities')->insert(['amenity_name' => 'フリーWifi']);
+        DB::table('amenities')->insert(['amenity_name' => 'ナイトウェア、パジャマ']);
+        DB::table('amenities')->insert(['amenity_name' => 'バスタオル、フェイスタオルなどタオル類']);
+        DB::table('amenities')->insert(['amenity_name' => 'シャンプー']);
+        DB::table('amenities')->insert(['amenity_name' => 'コンディショナー']);
+        DB::table('amenities')->insert(['amenity_name' => 'ボディーソープ']);
+        DB::table('amenities')->insert(['amenity_name' => 'シャワーキャップ']);
+        DB::table('amenities')->insert(['amenity_name' => 'ヘアゴム']);
+        DB::table('amenities')->insert(['amenity_name' => '歯ブラシ']);
+        DB::table('amenities')->insert(['amenity_name' => 'カミソリ']);
+        DB::table('amenities')->insert(['amenity_name' => 'ヘアブラシ']);
+        DB::table('amenities')->insert(['amenity_name' => 'スリッパ']);
+    }
+}
---
+++ database/seeders/AreaTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AreaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('areas')->insert(['area_name' => '北海道']);
+        DB::table('areas')->insert(['area_name' => '東北']);
+        DB::table('areas')->insert(['area_name' => '北関東']);
+        DB::table('areas')->insert(['area_name' => '首都圏']);
+        DB::table('areas')->insert(['area_name' => '甲信越']);
+        DB::table('areas')->insert(['area_name' => '東海']);
+        DB::table('areas')->insert(['area_name' => '伊豆・箱根']);
+        DB::table('areas')->insert(['area_name' => '北陸']);
+        DB::table('areas')->insert(['area_name' => '近畿']);
+        DB::table('areas')->insert(['area_name' => '四国']);
+        DB::table('areas')->insert(['area_name' => '山陽・山陰']);
+        DB::table('areas')->insert(['area_name' => '九州']);
+        DB::table('areas')->insert(['area_name' => '沖縄']);
+    }
+
+    /* php artisan db:seed --class=AreaTableSeeder  */
+}
