7be66db2bb03dafd8e9852b804a71153ec0b587e
新田野乃華 <b1017177@gmail.com>
2021-05-20 11:05:24 +0900

    朝食夕食付きを追加

--- database/seeders/PlansTableSeeder.php
+++ database/seeders/PlansTableSeeder.php
@@ -16,6 +16,7 @@ class PlansTableSeeder extends Seeder
     {
         //
         DB::table('plans')->insert(['plan_name'=>'朝食付き']);
+        DB::table('plans')->insert(['plan_name'=>'朝食・夕食付き']);
         DB::table('plans')->insert(['plan_name'=>'素泊まり']);
     }
 }
