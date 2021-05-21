4460798fd3a3b8e41fb4a587f000a0e37e7fdfc1
新田野乃華 <b1017177@gmail.com>
2021-05-21 10:43:11 +0900

    プラン作成・編集画面の作成（データ未連携）

--- resources/views/stores/plans/create.blade.php
+++ resources/views/stores/plans/create.blade.php
@@ -1,1 +1,18 @@
 {{-- プラン登録画面 --}}
+@extends('layout.app')
+@section('content')
+<br>
+<form action="">
+    @csrf
+    <div class="mb-3">
+      <label for="plan_name" class="form-label">プラン名</label>
+      <input type="text" class="form-control" id="plan_name" name="plan_name" placeholder="例）【記念日におすすめ】1泊朝食＋スパークリングワイン（モスカート・ペタロ）">
+    </div>
+    <div class="mb-3">
+        <label for="plan_description" class="form-label">説明</label>
+        <textarea class="form-control" id="plan_description" name="plan_description" rows="15" 
+        placeholder="例）内容，ご案内，注意事項など"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">登録する</button>
+</form>
+@endsection
--- resources/views/stores/plans/edit.blade.php
+++ resources/views/stores/plans/edit.blade.php
@@ -1,1 +1,17 @@
 {{-- プラン編集画面 --}}
+@extends('layout.app')
+@section('content')
+<br>
+<form action="">
+    @csrf
+    <div class="mb-3">
+      <label for="plan_name" class="form-label">プラン名</label>
+      <input type="text" class="form-control" id="plan_name" name="plan_name" >
+    </div>
+    <div class="mb-3">
+        <label for="plan_description" class="form-label">説明</label>
+        <textarea class="form-control" id="plan_description" name="plan_description" rows="10"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">登録する</button>
+</form>
+@endsection
