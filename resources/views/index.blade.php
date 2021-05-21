29a7dd9553c1a698e5ac38bd03210629cdeda0d2
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-05-22 00:41:20 +0900

    edited home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <div class="home-container">
     <div class="main-container row mx-0">
         <div class="col-5 offset-7 inoshikacho-wrapper">
-            <h1><span class="span1">猪鹿蝶</span><span class="span2">.travel</span></h1>
+            <h1><span class="span1">猪鹿蝶 </span><span class="span2">.travel</span></h1>
             <div class="row mx-0">
                 <div class="col-4 inoshikacho-item">
                     <i class="fas fa-search pb-4 w-100"></i>
