3db82d100016a4b0ab28aacf459da5685c0fe0a2
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-05-21 16:07:51 +0900

    added home page

--- resources/views/commons/nav.blade.php
+++ resources/views/commons/nav.blade.php
@@ -3,7 +3,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-default">
     <div class="container">
-      <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+      <a class="navbar-brand" href="/"><span class="span1">猪鹿蝶</span><span class="span2">.travel</span></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
