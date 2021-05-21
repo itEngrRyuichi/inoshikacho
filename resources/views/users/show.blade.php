438f41de7ad88d7da1ac7a4da8d6ab38b5c8fa1a
ami-2021php <2021php41-student015@la-study.com>
2021-05-21 16:22:24 +0900

    users/showの編集

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,9 +2,16 @@
 @section('content')
 {{-- サイト管理ユーザーの場合　会員詳細画面 --}}
 {{-- 店の管理ユーザー、会員ユーザーの場合　プロフィール --}}
+
+<div class="mx-auto" style="width: 500px;">
+
 <br>
-<h1>会員情報</h1>
+<h3>会員情報</h3>
 <br>
+
+<div class="input-group mb-3 row justify-content-center">
+  <img src="{{asset('images/no_user_image.png')}}" class="rounded-circle"   alt="" style="width: 100px;">
+</div>
 
 <table class="table">
     <thead>
@@ -20,7 +27,15 @@
         <td>一般会員 or 店舗</td>
       </tr>
       <tr>
+        <th scope="row">会員ID</th>
+        <td></td>
+      </tr>
+      <tr>
         <th scope="row">名前</th>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">生年月日</th>
         <td></td>
       </tr>
       <tr>
@@ -32,21 +47,24 @@
         <td></td>
       </tr>
       <tr>
-        <th scope="row">年齢</th>
-        <td></td>
-      </tr>
-      <tr>
         <th scope="row">メールアドレス</th>
         <td></td>
       </tr>
     </tbody>
 </table>
 
-<form action="">
+{{--新田さんがつくってくれたやつ 
+  <form action="">
     <button type="button" class="btn btn-success">編集する</button>
 </form>
 
 <br><br>
-<button type="button" class="btn btn-danger">退会する</button>
+<button type="button" class="btn btn-danger">退会する</button> --}}
 
+
+  <button type="button" class="btn btn-success">編集する</button>
+  <button type="button" class="btn btn-danger ">退会する</button>
+
+
+  
 @endsection
