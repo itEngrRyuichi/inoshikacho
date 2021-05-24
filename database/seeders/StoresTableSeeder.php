53b5a3ac9e9bc9bc4f48857e7acd391deb138943
sigayuusuke <yusuke.mczk0619@gmail.com>
2021-05-24 17:05:29 +0900

    とりあえず

---
+++ database/seeders/StoresTableSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StoresTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* DB::table('stores')->insert([
+            'store_name'=>'霧隠れの里',
+            'postal'=>'6695252',
+            'address'=>'兵庫県朝来市和田山町竹田古城山169',
+            'phone'=>'0796724003',
+            'email'=>'kiri@i.softbank.jp',
+            'description'=>'山間部の霧の深い場所に位置しているため敵から攻められにくいということに加え、初代水影の白蓮(びゃくれん)による秘密主義が重なり、非常に閉鎖的かつ排他的な発展を遂げている霧隠れ。水影の情報を知らない自国の忍がいるほどです。霧隠れの里で暮らす人々には特徴的な容姿をしている人も見られ、魚の外見的特徴を持った者が多数います。干柿鬼鮫(ほしがききさめ)に至ってはサメそのもののような容姿をしています。',
+            'access'=>'霧隠れの里前駅から徒歩４０分。当ホテル専用バス有',
+            'store_type_id'=>'2',
+            'area_id'=>'9',
+        ]);
+
+        DB::table('stores')->insert([
+            'store_name'=>'岩隠れの里',
+            'postal'=>'0283201',
+            'address'=>'岩手県花巻市大迫町内川目',
+            'phone'=>'0384937294',
+            'email'=>'iwa@i.softbank.jp',
+            'description'=>'岩隠れ(いわがくれ)の里は四尾と五尾を擁する“五大隠れ里”。五大国の1つ”土の国”に属する隠れ里で、“土影”を長としています。里の中の人物には特徴的な容姿をした者もいて、ダンゴ鼻をした者がよく見られます。',
+            'access'=>'岩隠れの里前駅からケーブルカーで５分。',
+            'store_type_id'=>'3',
+            'area_id'=>'2',
+        ]); */
+
+        /* DB::table('stores')->insert([
+            'store_name'=>'雨隠れの里',
+            'postal'=>'8914292',
+            'address'=>'鹿児島県熊毛郡 屋久島町小瀬田849番地20',
+            'phone'=>'0997435900',
+            'email'=>'ame@i.softbank.jp',
+            'description'=>'雨が多い気候で、内部はビルが乱立するごちゃごちゃとした近未来的な構造になっている。忍びの装いもゴーグルやシュノーケルマスク等を装着していたり雨傘やカラ傘を携えている者が多く、カラ傘は武器が仕込まれている。',
+            'access'=>'雨隠れの里前駅から徒歩１０分。洪水が予想されますので、駅に到着次第お迎えに上がります。',
+            'store_type_id'=>'12',
+            'area_id'=>'6',
+        ]); */
+        /* DB::table('stores')->insert([
+            'store_name'=>'木の葉隠れの里',
+            'postal'=>'3400043',
+            'address'=>'東京都渋谷区2-6-49',
+            'phone'=>'0489281367',
+            'email'=>'konoha@i.softbank.jp',
+            'description'=>'五大国の1つに数えられる「火の国」と呼ばれる国に存在する忍の隠れ里。 火影と呼ばれる者が長として里を治める。 主人公のうずまきナルトが暮らしている里で、頻繁にストーリーの舞台となる。',
+            'access'=>'渋谷駅から徒歩五分。',
+            'store_type_id'=>'5',
+            'area_id'=>'4',
+            ]); */
+        
+        /* DB::table('stores')->insert([
+            'store_name'=>'砂隠れの里',
+            'postal'=>'6890105',
+            'address'=>'鳥取県鳥取市福部町湯山２１６４−６６１',
+            'phone'=>'048121361',
+            'email'=>'suna@i.softbank.jp',
+            'description'=>'五大国の1つに数えられる「風の国」と呼ばれる国に存在する忍びの隠れ里。風影が長として治めている。 砂ばかりの土地で資源が少ないことや、風の国の大名が軍縮を薦めることから、国力の維持に苦悩させられている。',
+            'access'=>'砂隠れの里前駅から徒歩２０分。砂隠れの里前駅に到着した際、ご連絡いただければ、砂の車でお迎えに上がります。',
+            'store_type_id'=>'1',
+            'area_id'=>'11', */
+        /* ]); */
+    }
+}
