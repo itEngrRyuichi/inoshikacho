cb33a1d049da92fd46d9e218aafc3d5cf06f43f3
sigayuusuke <yusuke.mczk0619@gmail.com>
2021-05-27 14:55:12 +0900

    とりあえず

---
+++ database/seeders/StoresTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class StoresTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $store_name=array('旅館','プリンスホテル',)
+        $stores=array(
+            ['name'=>'草隠れの里','address'=>''],
+            ['name'=>'草隠れの里','address'=>''],
+            ['name'=>'草隠れの里','address'=>''],
+        );
+    }
+}
