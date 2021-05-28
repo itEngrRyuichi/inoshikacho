90cf9d2d5c8f5393fb308db5ea8905bf484ed8a0
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-05-28 09:55:50 +0900

    change for a while

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,12 +15,16 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //サイト管理ユーザー
-        $name_1 = ['カカシ', 'ナルト', 'サスケ', '春野サクラ', 'ヤマト', 
+        $name_1 = ['カカシ', 'ナルト', 'サスケ', '春野サクラ', 'ヤマト',
         'サイ', '猿飛アスマ', '奈良シカマル', '山中いの', '秋道チョウジ'];
         $name_1_alpha = ['kakashi', 'naruto', 'sasuke', 'sakura', 'yamato',
         'sai', 'asuma', 'shikmaru', 'ino', 'choji'];
+        $name_1_images = [
+            'images/users/hogehoge.jpg',
+            'images/users/hogehoge.jpg'
+        ];
 
-        for ($i=0; $i < count($name_1); $i++) { 
+        for ($i=0; $i < count($name_1); $i++) {
             $name=$name_1[$i];
             $name_alpha = $name_1_alpha[$i];
             $year=rand(1990,2010);
@@ -32,22 +36,22 @@ class UsersTableSeeder extends Seeder
             $address_2=rand(1,15);
             $address_3=rand(1,10);
         DB::table('users')->insert(['name'=> $name,
-                                    'type'=>1,
-                                    'birthday'=>"$year-$month-$date",
-                                    'email'=>"$name_alpha@example.com",
-                                    'phone'=>"080-$phone_1-$phone_2",
-                                    'password'=>$name_alpha,
-                                    'address'=>"東京都新宿区$address_1-$address_2-$address_3",
+                'type'=>1,
+                'birthday'=>"$year-$month-$date",
+                'email'=>"$name_alpha@example.com",
+                'phone'=>"080-$phone_1-$phone_2",
+                'password'=>$name_alpha,
+                'address'=>"東京都新宿区$address_1-$address_2-$address_3",
             ]);
         }
 
         //店舗管理ユーザー
-        $name_2 = ['夕日紅', '日向ヒナタ', '犬塚キバ', '油女シノ','マイト・ガイ', 
+        $name_2 = ['夕日紅', '日向ヒナタ', '犬塚キバ', '油女シノ','マイト・ガイ',
         'ロック・リー', '日向ネジ', 'テンテン', '千手柱間', '千手扉間'];
         $name_2_alpha=['kurenai', 'hinata', 'kiba','shino', 'gai',
         'ri', 'neji', 'tenten', 'hashirama', 'tobirama',];
 
-        for ($i=0; $i < count($name_2); $i++) { 
+        for ($i=0; $i < count($name_2); $i++) {
             $name=$name_2[$i];
             $name_alpha = $name_2_alpha[$i];
             $year=rand(1990,2010);
@@ -100,7 +104,7 @@ class UsersTableSeeder extends Seeder
         'orochimaru', 'kabuto', 'yoroi', 'jiroubo', 'kidoumaru',
         'sakon', 'tayuya', 'kimimaro', 'zabuza','shiro',
         'suigetsu', 'karin', 'jugo', 'madara', 'gamakichi',
-        'katsuyu', 'gato', 'teuchi', 'ayame', 'kinuta',  
+        'katsuyu', 'gato', 'teuchi', 'ayame', 'kinuta',
         'abumi', 'nagato', 'konan', 'itachi','kisame',
         'sasori', 'deidara', 'hidan', 'kakuto', 'zetsu',
     ];
@@ -116,7 +120,7 @@ class UsersTableSeeder extends Seeder
             $address_1=rand(1,10);
             $address_2=rand(1,10);
             $address_3=rand(1,10);
-    
+
         DB::table('users')->insert(['name'=> $name,
                                     'type'=>3,
                                     'birthday'=>"$year-$month-$date",
@@ -125,6 +129,6 @@ class UsersTableSeeder extends Seeder
                                     'password'=>$name_alpha,
                                     'address'=>"東京都千代田区$address_1-$address_2-$address_3",
         ]);
-        }  
+        }
     }
-}    
+}
